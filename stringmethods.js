a0fdd169a1b0377ce3f46bab01b74ffee234184e
Sam1231997 <opyotusamuel2020@gmail.com>
2024-07-07 01:07:42 +0100

    first files

---
+++ stringmethods.js
@@ -0,0 +1,111 @@
+ //let value;
+// string methods
+//const favouriteTeam="Chelsea FC";
+// value=favouriteTeam;
+// console.log(value);
+// // we use plus equal to to append things 
+// const sentenceprefix="the best team in the world";
+// sentenceprefix+="Plateau United";
+// value=sentenceprefix;
+// console.log(value);
+
+//Lowercase and uppercase
+//InedexOf method
+// let firstname="James";
+// let Lastname="Mathew";
+// let index=firstname.indexOf("e");
+
+//chartAt Method
+
+// Substring method
+// substring method can takes in two argument
+
+
+//slice methods
+
+
+//Escaping characters
+
+
+//converting strings into an array
+
+
+//includes methods
+//it returns the boolean true or false. checking for a word to show if the word exist or not
+
+//FUNCTION
+// function showHello(){
+//     console.log("Hello");
+// }
+// showHello();
+
+// //prompt
+// const username=prompt("what is your name?");
+// const userage=prompt("how old are you?");
+// function greetuser(username, userage){
+//     console.log(`welcome ${username}, your are ${userage} years old.`);
+// }
+
+
+
+// greetuser(username, userage);
+
+//class work
+// function addmult(a,b,c,d){
+//     console.log((a*b)+(c*d));
+// }
+// addmult(2,4,5,6);
+
+//annonymous function: it is a function that doesnt have a name
+// const sumNumber=function(a,b){
+//     console.log(a+b);
+
+// }
+// sumNumber(2,3);
+
+//iifes its a function that calls itself
+// (function showHello(){
+//     console.log("Hello")
+// })()
+
+//return keyword
+// function storage(){
+//     return "Fried Rice";
+
+// }
+// console.log(storage());
+
+
+// function storage(){
+//     return"Fried Rice";
+// }
+// const todaysmenu=storage();
+// console.log(todaysMenu);
+
+// function storage(){
+//     return"fried rice";
+// }
+
+// function basket(){
+//     const todaysmenu=storage();
+//     const availablemeal=`jollor 4id3 2ith ${todaysmenu} and chicken`;
+//     return availablemeal;
+// }
+// const food=basket();
+// console.log(food);
+
+
+//welcome to array
+//arrays and arrays methods
+// let value;
+// const fruits=["Banana", "Cherr", "Mango",[2,4],{}];
+// value=fruits.length;
+// console.log(value);
+
+//Numbers methods 
+// const imposterarray=[55,1,2,3,20,3,49,5,6,7,69,79];
+// imposterarray.sort(function(x,y){
+//     return y-x;
+// })
+// value=imposterarray;
+// console.log(value);
